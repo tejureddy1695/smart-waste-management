ccae39edcf9b34c219667aa0407ae6e348e74102
tejureddy1695 <tejureddy699@gmail.com>
2025-11-10 22:19:50 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || "https://smart-waste-management-backend-2eqj.onrender.com"
+const API_URL = "https://smart-waste-management-backend-2eqj.onrender.com";
+
 
 const api = axios.create({
   baseURL: API_URL,
